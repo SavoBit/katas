89a48e72ecd8e70e5b74dc2b18e12b7d6043506e
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 00:34:55 -0800

    Updated

--- install-enforcerd-on-k8s/assets/post-account.sh
+++ install-enforcerd-on-k8s/assets/post-account.sh
@@ -5,8 +5,8 @@ while ! kubectl version > /dev/null 2>&1; do sleep 1; done
 echo "OK"
 echo
 
-helm init
-helm repo add aporeto https://charts.aporeto.com/_unstable/clients
+helm init > /dev/null 2>&1
+helm repo add aporeto https://charts.aporeto.com/_unstable/clients > /dev/null 2>&1
 
 echo "Waiting for helm to be ready..."
 while ! helm version > /dev/null 2>&1; do sleep 1; done
