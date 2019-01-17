2c88da7159b24d97506b7c43c19852fc0e36a9b4
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-16 16:21:49 -0800

    Updated

---
+++ k8s-playground/assets/setup1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+launch.sh &
+pid="$!"
+
+curl https://download.aporeto.com/files/apoctl/linux/apoctl-v1.235.2 -o /usr/local/bin/apoctl
+chmod +x /usr/local/bin/apoctl
+
+
+wait "$pid"
