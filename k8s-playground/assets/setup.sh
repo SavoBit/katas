b53ab350bd8a2c7f5d220bd37edaafb6116f0d3a
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-16 16:38:03 -0800

    Updated

--- k8s-playground/assets/setup.sh
+++ k8s-playground/assets/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+echo "toto" > /opt/hello
 launch.sh &
 pid="$!"
 
 curl https://download.aporeto.com/files/apoctl/linux/apoctl-v1.235.2 -o /tmp/apoctl
 chmod +x /tmp/apoctl
-
 
 wait "$pid"
