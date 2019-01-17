cf22128d5691c15b3531e229fc4751c14d93c08a
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 02:04:08 -0800

    Updated

--- playground-k8s/assets/post-account.sh
+++ playground-k8s/assets/post-account.sh
@@ -4,3 +4,11 @@ alias ks="kubectl -n kube-system"
 alias k="kubectl -n"
 
 /opt/common-deploy-on-k8s.sh
+
+echo
+echo "You can check your enforcerd at:"
+echo
+echo "platform:  $DEFAULT_CLAD_URL/app/visualize?namespace=$KATACODA_SESSION_ID"
+echo "enforcers: $DEFAULT_CLAD_URL/app/enforcers?namespace=$KATACODA_SESSION_ID"
+echo "policies:  $DEFAULT_CLAD_URL/app/networkaccesspolicies?namespace=$KATACODA_SESSION_ID"
+echo
