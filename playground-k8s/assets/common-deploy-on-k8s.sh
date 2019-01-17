e51c20d8d32480d773afb667f981c102e1ece069
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 02:15:04 -0800

    Updated

--- playground-k8s/assets/common-deploy-on-k8s.sh
+++ playground-k8s/assets/common-deploy-on-k8s.sh
@@ -3,9 +3,9 @@
 
 # DO NOT EDIT OUTSIDE OD /common folder
 
-echo "Waiting for Katacoda to cacth up (this can take a minute or two)..."
+echo "Waiting for Kubernetes to stabilize..."
 while ! kubectl version > /dev/null 2>&1; do sleep 1; done
-echo "* kubernetes: OK"
+echo "* api: OK"
 
 helm init > /dev/null 2>&1
 helm repo add aporeto https://charts.aporeto.com/_unstable/clients > /dev/null 2>&1
@@ -13,6 +13,8 @@ helm repo add aporeto https://charts.aporeto.com/_unstable/clients > /dev/null 2
 while ! helm version > /dev/null 2>&1; do sleep 1; done
 echo "* helm: OK"
 
+echo
+echo "Deploying Aporeto..."
 apoctl appcred create aporeto-operator \
   --role @auth:role=aporeto-operator \
   --type k8s | kubectl apply -f - -n kube-system > /dev/null || exit 1
