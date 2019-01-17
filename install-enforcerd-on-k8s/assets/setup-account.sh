2527b48c27a4851f38bf83463ff42fab8f7c2a7b
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-16 22:13:55 -0800

    Updated

--- install-enforcerd-on-k8s/assets/setup-account.sh
+++ install-enforcerd-on-k8s/assets/setup-account.sh
@@ -18,8 +18,12 @@ prompt APORETO_ACCOUNT  "Aporeto Account Name"
 prompt APOCTL_NAMESPACE "Namespace"             "/$APORETO_ACCOUNT"
 prompt APOCTL_API       "API URL"               "$DEFAULT_API_URL"
 
+echo "We will now retrieve an api token."
+eval "$(apoctl auth aporeto --account $APOCTL_ACCOUNT -e)"
+
 cat << EOF > ~/.aporeto
 export APORETO_ACCOUNT=$APORETO_ACCOUNT
 export APOCTL_NAMESPACE=$APOCTL_NAMESPACE
+export APOCTL_TOKEN=$APOCTL_TOKEN
 export APOCTL_API=$APOCTL_API
 EOF
