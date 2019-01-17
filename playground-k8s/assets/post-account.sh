698f87d22f1fe552ff529dd0bd665152ad0c359d
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 02:17:00 -0800

    Updated

--- playground-k8s/assets/post-account.sh
+++ playground-k8s/assets/post-account.sh
@@ -8,7 +8,11 @@ alias k="kubectl -n"
 echo
 echo "Useful links:"
 echo
-echo "platform:  $DEFAULT_CLAD_URL/app/visualize?namespace=$APOCTL_NAMESPACE"
-echo "enforcers: $DEFAULT_CLAD_URL/app/enforcers?namespace=$APOCTL_NAMESPACE"
-echo "policies:  $DEFAULT_CLAD_URL/app/networkaccesspolicies?namespace=$APOCTL_NAMESPACE"
+echo "* dashboard: $DEFAULT_CLAD_URL/app/visualize?dashboard=$APOCTL_NAMESPACE"
+echo "* workloads: $DEFAULT_CLAD_URL/app/visualize?namespace=$APOCTL_NAMESPACE"
+echo "* enforcers: $DEFAULT_CLAD_URL/app/enforcers?namespace=$APOCTL_NAMESPACE"
+echo "* policies:  $DEFAULT_CLAD_URL/app/networkaccesspolicies?namespace=$APOCTL_NAMESPACE"
+echo "* networks:  $DEFAULT_CLAD_URL/app/externalservices?namespace=$APOCTL_NAMESPACE"
+
 echo
+echo "Ready."
