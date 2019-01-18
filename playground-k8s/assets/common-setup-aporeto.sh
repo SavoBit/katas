6a842e39d0ed6482cd03fe96739061551146412e
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 22:17:44 -0800

    Updated

--- playground-k8s/assets/common-setup-aporeto.sh
+++ playground-k8s/assets/common-setup-aporeto.sh
@@ -8,10 +8,15 @@ DEFAULT_RELEASE="3.5.0-r5"
 marked_release=$(cat /etc/APORETO_RELEASE)
 APORETO_RELEASE=${marked_release:-$DEFAULT_RELEASE}
 echo "$APORETO_RELEASE" > /etc/APORETO_RELEASE
+echo "Configuring version $APORETO_RELEASE"
 
 # get apoctl
+echo "> Downloading apoctl"
 curl -sSL "https://download.aporeto.com/releases/release-$APORETO_RELEASE/apoctl/linux/apoctl" -o /usr/local/bin/apoctl
 chmod +x /usr/local/bin/apoctl
 
 # start k8s
+echo "> Starting Kubernetes"
 launch.sh &
+
+echo "Now, please type '/opt/common-setup.sh && source ~/.aporeto' to update the Katacoda environment."
--- playground-k8s/assets/common-switch-release.sh
+++ playground-k8s/assets/common-switch-release.sh
@@ -11,7 +11,3 @@ echo "Switching from $APORETO_RELEASE_CURRENT to $APORETO_RELEASE"
 
 echo "$APORETO_RELEASE" > /etc/APORETO_RELEASE
 /opt/common-setup-aporeto.sh
-
-echo
-echo "Switched to $APORETO_RELEASE."
-echo "type '/opt/common-setup.sh' to update the katacoda environment."
