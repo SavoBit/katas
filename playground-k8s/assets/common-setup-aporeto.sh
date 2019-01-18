d1d1b688f39e113bb910116698f4dd3a84baad46
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 22:13:37 -0800

    Updated

--- playground-k8s/assets/common-setup-aporeto.sh
+++ playground-k8s/assets/common-setup-aporeto.sh
@@ -2,18 +2,16 @@
 
 # DO NOT EDIT OUTSIDE OD /common folder
 
-# Set the aporeto release here.
-# that's all.
+# Set the aporeto release here. That's all.
 DEFAULT_RELEASE="3.5.0-r5"
 
 marked_release=$(cat /etc/APORETO_RELEASE)
 APORETO_RELEASE=${marked_release:-$DEFAULT_RELEASE}
-
-# start k8s
-launch.sh &
+echo "$APORETO_RELEASE" > /etc/APORETO_RELEASE
 
 # get apoctl
 curl "https://download.aporeto.com/releases/release-$APORETO_RELEASE/apoctl/linux/apoctl" -o /usr/local/bin/apoctl
 chmod +x /usr/local/bin/apoctl
 
-echo "${APORETO_RELEASE:-$RELEASE}" > /etc/APORETO_RELEASE
+# start k8s
+launch.sh &
