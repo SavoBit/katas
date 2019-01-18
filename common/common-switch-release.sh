b71f947cd68c98eda63b11ef8ec05b6141fa82c0
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 22:19:03 -0800

    Updated

--- common/common-switch-release.sh
+++ common/common-switch-release.sh
@@ -5,6 +5,8 @@ APORETO_RELEASE_CURRENT="$(cat /etc/APORETO_RELEASE)"
 if [[ "$1" == "" ]]; then
     echo -n "Release version? (current: $APORETO_RELEASE_CURRENT): "
     read -r APORETO_RELEASE
+else
+    APORETO_RELEASE="$1"
 fi
 
 echo "Switching from $APORETO_RELEASE_CURRENT to $APORETO_RELEASE"
