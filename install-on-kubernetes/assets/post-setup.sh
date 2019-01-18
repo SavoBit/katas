3088c54a6607ecc2b0bd915b43d4be094b90230b
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-18 02:25:22 -0800

    Updated

--- install-on-kubernetes/assets/post-setup.sh
+++ install-on-kubernetes/assets/post-setup.sh
@@ -6,7 +6,7 @@ echo "OK"
 echo
 
 helm init > /dev/null 2>&1
-helm repo add aporeto https://charts.aporeto.com/_unstable/clients > /dev/null 2>&1
+helm repo add aporeto https://charts.aporeto.com/clients > /dev/null 2>&1
 
 echo "Waiting for helm to be ready..."
 while ! helm version > /dev/null 2>&1; do sleep 1; done
