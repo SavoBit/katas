e1948449fe68e8c933d7f4dbb89ecc708265068a
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-17 21:58:47 -0800

    Updated

--- install-on-kubernetes/assets/common-setup-aporeto.sh
+++ install-on-kubernetes/assets/common-setup-aporeto.sh
@@ -2,7 +2,12 @@
 
 # DO NOT EDIT OUTSIDE OD /common folder
 
-APORETO_RELEASE=${"$APORETO_RELEASE":-"3.5.0-r5"}
+# Set the aporeto release here.
+# that's all.
+DEFAULT_RELEASE="3.5.0-r5"
+
+marked_release=$(cat /etc/APORETO_RELEASE)
+APORETO_RELEASE=${marked_release:-$DEFAULT_RELEASE}
 
 # start k8s
 launch.sh &
@@ -11,4 +16,4 @@ launch.sh &
 curl "https://download.aporeto.com/releases/release-$APORETO_RELEASE/apoctl/linux/apoctl" -o /usr/local/bin/apoctl
 chmod +x /usr/local/bin/apoctl
 
-echo "$APORETO_RELEASE" > /etc/APORETO_RELEASE
+echo "${APORETO_RELEASE:-$RELEASE}" > /etc/APORETO_RELEASE
