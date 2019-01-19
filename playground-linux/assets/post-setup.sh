484d1512241f9fb49b9c60109afb19fcaf3c6e3f
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-19 00:01:10 -0800

    Updated

--- playground-linux/assets/post-setup.sh
+++ playground-linux/assets/post-setup.sh
@@ -2,3 +2,4 @@
 
 echo "> Downloading enforcerd"
 curl -sSL "https://download.aporeto.com/releases/$APORETO_RELEASE/enforcerd/linux/enforcerd" -o /usr/local/bin/enforcerd
+chmod +x /usr/local/bin/enforcerd
