13d189fc70bdd796c4969822c3c34ff60171efce
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-21 15:00:55 -0800

    Updated

--- playground-linux/assets/post-setup.sh
+++ playground-linux/assets/post-setup.sh
@@ -2,21 +2,23 @@
 
 echo "> Downloading enforcerd"
 
-curl http://download.aporeto.com/aporeto-packages.gpg | apt-key add -
-echo "deb https://repo.aporeto.com/deb/apt aporeto main" | sudo tee /etc/apt/sources.list.d/aporeto.list
+(
+    curl http://download.aporeto.com/aporeto-packages.gpg | apt-key add -
+    echo "deb https://repo.aporeto.com/deb/apt aporeto main" | sudo tee /etc/apt/sources.list.d/aporeto.list
 
-apt update > /dev/null
-apt -y install enforcerd > /dev/null
+    apt update > /dev/null
+    apt -y install enforcerd > /dev/null
 
-cat << EOF > /etc/enforcerd.conf
+    cat << EOF > /etc/enforcerd.conf
 # this notation is deprecateds
 ENFORCERD_SQUALL_NAMESPACE=$APOCTL_NAMESPACE
 ENFORCERD_NAMESPACE=$APOCTL_NAMESPACE
 EOF
 
-mkdir -p /var/lib/aporeto
-apoctl appcred create enforcerd \
-  --role @auth:role=enforcer \
-  > /var/lib/aporeto/default.creds > /dev/null
+    mkdir -p /var/lib/aporeto
+    apoctl appcred create enforcerd \
+    --role @auth:role=enforcer \
+    > /var/lib/aporeto/default.creds
 
-systemctl restart enforcerd
+    systemctl restart enforcerd
+) > /opt/aporeto-init.log
