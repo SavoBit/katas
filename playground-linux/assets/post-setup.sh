fc04460a66f8673646ee70b1350dc3f013b10434
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-21 14:55:20 -0800

    Updated

--- playground-linux/assets/post-setup.sh
+++ playground-linux/assets/post-setup.sh
@@ -6,7 +6,15 @@ curl http://download.aporeto.com/aporeto-packages.gpg | apt-key add -
 echo "deb https://repo.aporeto.com/deb/apt aporeto main" | sudo tee /etc/apt/sources.list.d/aporeto.list
 apt update
 
+apt install enforcerd
+
 cat << EOF > /etc/enforcerd.conf
-ENFORCERD_API=$APOCTL_API
+# this notation is deprecateds
 ENFORCERD_SQUALL_NAMESPACE=$APOCTL_NAMESPACE
+ENFORCERD_NAMESPACE=$APOCTL_NAMESPACE
 EOF
+
+mkdir -p /var/lib/aporeto
+apoctl appcred create enforcerd \
+  --role @auth:role=enforcer \
+  > /var/lib/aporeto/default.creds
